24c1c2fefeca82a00bd7632623307b89bcdb9567
Priyo61 <95367811+Priyo61@users.noreply.github.com>
2023-06-29 02:16:08 +0530

    simple project
    Translation English to Manipuri

---
+++ git.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+word={'land':'lam','mom':'ema','papa':'baba','love':'nungshi'}
+option=word.keys()
+print('look option to translate english to manipuri',option)
+a=input('Please look option to know meaning of given words:-')
+print('The meaning of given word is',word[a])
+
+
+# In[ ]:
+
+
+
+
